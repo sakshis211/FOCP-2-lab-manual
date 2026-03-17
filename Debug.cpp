d9a5713479b35fb0a742b1b038f6c877fa0a328e
sakshis211 <sakshisaini99588@gmail.com>
2026-03-17 18:08:26 +0530

    Create Debug.cpp

---
+++ Debug.cpp
@@ -0,0 +1,53 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n, sum = 0; 
+
+//     cin >> n;
+
+//     for(int i = 1; i <= n; i++) {
+//         sum = sum + i;
+//     }
+
+//     cout << sum;
+
+//     return 0;
+// }
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n = 4;
+
+    for(int i = n; i >= 1; i--) {
+
+        for(int s = 1; s <= n - i; s++)
+            cout << " ";
+
+        for(int j = 1; j <= 2*i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+
+    for(int i = 2; i <= n; i++) {
+
+        for(int s = 1; s <= n - i; s++)
+            cout << " ";
+
+        for(int j = 1; j <= 2*i - 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
