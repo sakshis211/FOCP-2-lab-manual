4541e3643d4186b7248e5751f78ae04bd243e940
sakshis211 <sakshisaini99588@gmail.com>
2026-05-15 11:01:35 +0530

    Create q,15.cpp

---
+++ q,15.cpp
@@ -0,0 +1,26 @@
+// #include<iostream>
+// using namespace std;
+
+// class Check
+// {
+// public:
+//     void countNum()
+//     {
+//         int a[5],c=0;
+
+//         for(int i=0;i<5;i++)
+//         cin>>a[i];
+
+//         for(int i=0;i<5;i++)
+//             if(a[i]%3==0 && a[i]%5==0)
+//             c++;
+
+//         cout<<"Count="<<c;
+//     }
+// };
+
+// int main()
+// {
+//     Check obj;
+//     obj.countNum();
+// }
